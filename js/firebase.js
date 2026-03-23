ce2e69cb3714e53cb2852fa567a4d6292a7843aa
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2026-03-23 15:31:03 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBNWSQA2-p3eJqeqmxOCxnrhBKkZMTpOxg",
-    authDomain: "ktc-diest-2791b.firebaseapp.com",
-    projectId: "ktc-diest-2791b",
-    storageBucket: "ktc-diest-2791b.firebasestorage.app",
-    messagingSenderId: "264576003118",
-    appId: "1:264576003118:web:37bc5d07b1b3b2db1fc3bf"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBNWSQA2-p3eJqeqmxOCxnrhBKkZMTpOxg",
+  authDomain: "ktc-diest-2791b.firebaseapp.com",
+  projectId: "ktc-diest-2791b",
+  storageBucket: "ktc-diest-2791b.firebasestorage.app",
+  messagingSenderId: "264576003118",
+  appId: "1:264576003118:web:37bc5d07b1b3b2db1fc3bf"
+};
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export {auth, app, storage };
