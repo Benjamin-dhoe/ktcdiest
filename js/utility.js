a8c35bdd1d6105282c8d4af5079c36dab665a769
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2026-03-25 08:22:52 +0100

    Update utility.js

--- js/utility.js
+++ js/utility.js
@@ -8,3 +8,14 @@ export function getCookie(name) {
   }
   return null;
 }
+
+function slugify(text) {
+  return text
+    .toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
